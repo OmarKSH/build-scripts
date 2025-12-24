41a3a3b34cd4409d635368548f5441cfbc8fd0fe
OmarKSH <7662668+OmarKSH@users.noreply.github.com>
2025-12-24 14:41:01 +0000

    update android-app.sh script with a better build image

--- other-scripts/android-app.sh
+++ other-scripts/android-app.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 #https://github.com/mingchen/docker-android-build-box
-docker run --rm -v `pwd`:/project android-sdk bash -c 'cd /project; ./gradlew build'
+#docker run --rm -v `pwd`:/project android-sdk bash -c 'cd /project; ./gradlew build'
+#docker run --rm -v `pwd`:/project saschpe/android-sdk:35-jdk23.0.2_7 bash -c 'cd /project; ./gradlew build'
+docker run --rm -v `pwd`:/project alvrme/alpine-android:android-29-jdk21 bash -c 'cd /project; ./gradlew build'
